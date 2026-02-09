ad4e4a13ca2c6b9eff1252f725db833d0ffcb323
satyam-ku <rajsatyam2005@gmail.com>
2026-02-09 11:59:32 +0530

    add join and pipeline querry

--- q18.js
+++ q18.js
@@ -20,3 +20,48 @@ db.employees.aggregate(
 
     ]
 )
+
+//
+db.courses.aggregate(
+    [
+        {
+            $lookup: {
+            from: "modules",
+            let:{courseId: "$_id"},
+            pipeline:
+                [
+                    {$match: { $expr:{ $eq:["$courseId", "$$courseId"]}}}
+                ],
+                as : "modules"
+            }
+        }
+        
+    ]
+)
+
+db.courses.aggregate(
+    [
+        {
+            $lookup: {
+            from: "modules",
+            let:{courseId: "$_id"},
+            pipeline:
+                [
+                    {$match: { $expr:{ $eq:["$courseId", "$$courseId"]}}},
+                    {$lookup:
+                        {
+                            from: "lessons",
+                            let : {moduleId:"$_id"},
+                            pipeline: [
+                               {$match: { $expr:{ $eq:["$moduleId", "$$moduleId"]}}} 
+                            ],
+                            as: "lessons"
+                        }
+                    }
+                ],
+                as : "modules"
+            }
+        }
+        
+    ]
+)
